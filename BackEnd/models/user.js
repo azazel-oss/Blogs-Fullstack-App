9a585b5ccf27e11840d3b38a0b1ca884eb78416f
Asad Mahmood <asadmhmd1234@gmail.com>
2022-07-24 18:16:10 +0530

    Add token generation, auth methods in user model

--- BackEnd/models/user.js
+++ BackEnd/models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -21,8 +23,44 @@ const UserSchema = new Schema({
   },
 });
 
+UserSchema.pre("save", function (next) {
+  const user = this;
+  if (!user.isModified("password")) return next();
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err);
+
+      user.password = hash;
+      next();
+    });
+  });
+});
+
 UserSchema.virtual("fullName").get(function () {
   return `${this.firstName} ${this.lastName}`;
 });
+
+UserSchema.methods.comparePassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+UserSchema.methods.generateJWT = function () {
+  const today = new Date();
+  const expirationDate = new Date(today);
+  expirationDate.setDate(today.getDate() + 60);
+
+  let payload = {
+    _id: this._id,
+    username: this.username,
+    firstName: this.firstName,
+    lastName: this.lastName,
+  };
+
+  return jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: parseInt(expirationDate.getTime() / 1000, 10),
+  });
+};
 
 module.exports = mongoose.model("User", UserSchema);
