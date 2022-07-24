48a12a3aa64133fd4410134e20bcabae01d3763a
Asad Mahmood <asadmhmd1234@gmail.com>
2022-07-24 18:17:25 +0530

    Update server file to include new dependancies

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 var express = require("express");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const passport = require("passport");
+const passportJWT = require("./middlewares/jwt");
 
 // Database configuration
 const mongoose = require("mongoose");
@@ -15,6 +17,7 @@ db.on("error", console.error.bind(console, "mongo connection error"));
 
 const blogRouter = require("./routes/blog");
 const commentRouter = require("./routes/comment");
+const authRouter = require("./routes/auth");
 
 var app = express();
 
@@ -22,6 +25,8 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(passport.initialize());
+passportJWT(passport);
 
 app.use("/api/blogs", blogRouter);
 app.use("/api/blogs/:blogId/comments", commentRouter);
